01c517c8f575f6b8d81a0553e562be6caf576891
Emil Mieilica <emmiei@microsoft.com>
2024-07-30 20:44:09 +0200

    First version that dumps events to console

---
+++ src/etwdump/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Session;
+
+namespace etwdump;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        if (args is [])
+        {
+            Console.WriteLine($"No provider to listen to. Use '{System.AppDomain.CurrentDomain.FriendlyName} <guid>'. Example: '{System.AppDomain.CurrentDomain.FriendlyName} 5a02809f-fbb3-4874-a478-65d401f49b8d'");
+            return 0;
+        }
+
+        Console.WriteLine($"Listening events from {string.Join(", ", args)}");
+
+        using var etwSession = new TraceEventSession("MyEtwLog", null);
+        foreach (var provider in args)
+        {
+            etwSession.EnableProvider(
+                providerGuid: Guid.Parse(provider),
+                providerLevel: TraceEventLevel.Always,
+                options: new TraceEventProviderOptions()
+                {
+                    Arguments =
+                    [
+                        new KeyValuePair<string, string>("FilterAndPayloadSpecs", "[AS]*\r\n*")
+                    ],
+                });
+        }
+        etwSession.Source.Dynamic.All += data => Console.WriteLine($"H: {data.ProviderName} - {data.EventName}: {string.Join(" ", data.PayloadNames.Take(8).Select(name => $"{name}={data.PayloadStringByName(name)}"))}");
+
+        Console.CancelKeyPress += (_, _) => etwSession.Dispose();
+
+        etwSession.Source.Process();
+
+        Console.WriteLine("Good bye!");
+
+        return 0;
+    }
+}
